5aa3f69f3efa5101472a720f92192cc8af05f4ae
mccreate <minwoo6951@naver.com>
2022-01-10 15:46:10 +0900

    1.3

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import "fmt"
 
+//Go needs Data Type in below.
+/*
+   func funcName(argv1 argv1's Datatype, argv2 argv2's Datatype, ...) return's DataType {
+	   return blah
+   }
+*/
+func multiply(a int, b int) int {
+	return a * b
+}
+
 func main() {
-	name := "mccreate"
-	name = "Min Woo"
-	fmt.Println(name)
+	fmt.Println(multiply(2, 2))
 }
