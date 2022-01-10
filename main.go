702b19d6057f1d7b937dc0d0861bdc82b67be975
mccreate <minwoo6951@naver.com>
2022-01-10 17:59:49 +0900

    Arrays and Slices

--- main.go
+++ main.go
@@ -2,20 +2,27 @@ package main
 
 import "fmt"
 
-// Pointer
+// list
 
 func main() {
-	a := 2
-	// linking b addr to a addr
-	b := &a
-	a = 10
-	// you can see memory addr with & keyword
-	fmt.Println(&a, &b)
+	// how to make list?
+	// list_name := [list_size]Data_Type{"your", "list", "element"}
+	names := [3]string{"mc", "cre", "ate"}
+	names2 := [3]string{"mc"}
+	numbers := [3]int{1, 2}
 
-	// you can see value in memory addr * keyword
-	fmt.Println(a, *b)
+	// OverFlow!!
+	// names[3] = "Overflow String"
+	fmt.Println(names)
+	fmt.Println(names2)
+	fmt.Println(numbers)
 
-	// you can change a val with *b (if you link a with b)
-	*b = 20
-	fmt.Println(a, *b)
+	// You can assign non-limited length list
+	// list_name := []Data_type{list_elements}
+	non_limited_list := []int{1, 2, 3, 4, 5}
+	fmt.Println(non_limited_list)
+
+	// append Example
+	non_limited_list = append(non_limited_list, 4)
+	fmt.Println(non_limited_list)
 }
