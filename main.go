a728a20c66e9d2c514e7f240427798141af4141b
mccreate <minwoo6951@naver.com>
2022-01-10 17:28:29 +0900

    Pointers!

--- main.go
+++ main.go
@@ -2,32 +2,20 @@ package main
 
 import "fmt"
 
-// switch
-
-func canIDrink1(age int) bool {
-	switch age {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
-
-// you can use "switch" right this.
-func canIDrink2(age int) bool {
-	switch {
-	case age < 18:
-		return false
-	case age == 18:
-		return true
-	case age > 50:
-		return false
-	}
-	return false
-}
+// Pointer
 
 func main() {
-	fmt.Println(canIDrink1(16))
-	fmt.Println(canIDrink2(16))
+	a := 2
+	// linking b addr to a addr
+	b := &a
+	a = 10
+	// you can see memory addr with & keyword
+	fmt.Println(&a, &b)
+
+	// you can see value in memory addr * keyword
+	fmt.Println(a, *b)
+
+	// you can change a val with *b (if you link a with b)
+	*b = 20
+	fmt.Println(a, *b)
 }
