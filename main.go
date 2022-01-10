7d2b87253122a3f6786ac7e3eab72a9cc15c061f
mccreate <minwoo6951@naver.com>
2022-01-10 15:57:20 +0900

    Functions Part One

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-//Go needs Data Type in below.
+// Go needs Data Type in below.
 /*
    func funcName(argv1 argv1's Datatype, argv2 argv2's Datatype, ...) return's DataType {
 	   return blah
@@ -15,18 +15,25 @@ func multiply(a int, b int) int {
 	return a * b
 }
 
-//Go can return many argvs.
+// Go can return many argvs.
 func lenAndUpper(name string) (int, string) {
 	return len(name), strings.ToUpper(name)
+}
+
+// When there are many func arguments, You can shorten using "..." keyword.
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
 
 func main() {
 	fmt.Println(multiply(2, 2))
 	totalLength, upperName := lenAndUpper("mccreate")
-	//Go must be use all variable.
+	// Go must be use all variable.
 	fmt.Println(totalLength, upperName)
 
-	//You can ignore Variable declared but not used in below.
+	// You can ignore Variable declared but not used in below.
 	exLength, _ := lenAndUpper("mccreate")
 	fmt.Println(exLength)
+
+	repeatMe("mc", "cre", "ate")
 }
