e22195577476cb68e680900b34ae59b93df799b4
mccreate <minwoo6951@naver.com>
2022-01-14 10:56:45 +0900

    hitURL

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 var errRequestFailed = errors.New("Request failed.")
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -21,17 +22,21 @@ func main() {
 		"https://academy.nomadcoders.co/",
 	}
 	for _, url := range urls {
+		result := "OK"
 		err := hitURL(url)
 		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(" Connected")
+			result = "FAILED"
 		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
 func hitURL(url string) error {
-	fmt.Print("Checking:", url)
+	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
 		return errRequestFailed
