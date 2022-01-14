bb0202653a2e9389002421b120a8f66eb342085b
mccreate <minwoo6951@naver.com>
2022-01-14 12:03:05 +0900

    Fast URL Checker

--- main.go
+++ main.go
@@ -5,13 +5,14 @@ import (
 	"net/http"
 )
 
-type result struct {
+type requestResult struct {
 	url    string
 	status string
 }
 
 func main() {
-	c := make(chan result)
+	c := make(chan requestResult)
+	results := map[string]string{}
 
 	urls := []string{
 		"https://www.airbnb.com/",
@@ -24,18 +25,28 @@ func main() {
 		"https://www.instagram.com/",
 		"https://academy.nomadcoders.co/",
 	}
+
 	for _, url := range urls {
 		go hitURL(url, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
 // Send only
-func hitURL(url string, c chan<- result) {
-	fmt.Println("Checking:", url)
+func hitURL(url string, c chan<- requestResult) {
+
 	resp, err := http.Get(url)
 	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
 		status = "FAILED"
 	}
-	c <- result{url: url, status: status}
+	c <- requestResult{url: url, status: status}
 }
